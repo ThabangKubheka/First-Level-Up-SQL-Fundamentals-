54c9d99dc60e92b527044129c161780b486043b5
Noxolo Duma <noxolo@bbd.co.za>
2023-02-20 23:14:31 +0200

    SQL Tables with ALTEREd tableZ

--- resources/src/tables.sql
+++ resources/src/tables.sql
@@ -1,14 +1,25 @@
+CREATE TABLE Author(
+author_id int UNIQUE IDENTITY(1,1) NOT NULL,
+firstname varchar(50) NOT NULL,
+lastname varchar(50) NOT NULL,
+);
+
+
+CREATE TABLE Publisher(
+publisher_id int UNIQUE IDENTITY (1,1) NOT NULL,
+name varchar(50) NOT NULL
+);
+
 CREATE TABLE Book(
 book_id int UNIQUE IDENTITY (1,1) NOT NULL,
 title varchar(50) NOT NULL,
-author_id int NOT NULL,
 publisher_id int NOT NULL,
 publish_date datetime NOT NULL,
 isbn int NOT NULL,
 quantity int NOT NULL,
-author_id int FOREIGN KEY REFERENCES Author(author_id),
-publisher_id int FOREIGN KEY REFERENCES Publisher(publisher_id)
 );
+ALTER TABLE Book
+ADD CONSTRAINT fk_publisher_id FOREIGN KEY (publisher_id) REFERENCES Publisher (publisher_id);
 
 CREATE TABLE Member(
 member_id int UNIQUE IDENTITY(1,1) NOT NULL,
@@ -19,16 +30,6 @@ cellphone_number char(10) NOT NULL,
 status bit NOT NULL
 );
 
-CREATE TABLE Author(
-author_id int UNIQUE IDENTITY(1,1) NOT NULL,
-firstname varchar(50) NOT NULL,
-lastname varchar(50) NOT NULL,
-);
-
-CREATE TABLE Publisher(
-publisher_id int UNIQUE IDENTITY (1,1) NOT NULL,
-name varchar(50) NOT NULL
-);
 
 CREATE TABLE Genre (
 genre_id int UNIQUE IDENTITY (1,1) NOT NULL,
@@ -39,11 +40,14 @@ description varchar(140)
 
 CREATE TABLE Book_Author(
 book_author_id int UNIQUE IDENTITY (1,1) NOT NULL,
-author_id int NOT NULL
+author_id int NOT NULL,
 book_id int NOT NULL,
-author_id int FOREIGN KEY REFERENCES Author(author_id),
-book_id int FOREIGN KEY REFERENCES Book(book_id)
 );
+ALTER TABLE Book_Author
+ADD CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES Author (author_id);
+
+ALTER TABLE Book_Author
+ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book (book_id);
 
 
 CREATE TABLE Rental(
@@ -51,16 +55,20 @@ rental_id int UNIQUE IDENTITY (1,1) NOT NULL,
 book_id int NOT NULL,
 member_id int NOT NULL, 
 loan_date datetime NOT NULL,
-member_id int FOREIGN KEY REFERENCES Member(member_id),
-book_id int FOREIGN KEY REFERENCES Book(book_id)
 );
-
+ALTER TABLE Rental
+ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book (book_id);
+ALTER TABLE Rental
+ADD CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES Member (member_id);
 
 CREATE TABLE Book_Genre(
 book_genre_id int UNIQUE IDENTITY (1,1) NOT NULL,
 genre_id int NOT NULL,
 book_id int NOT NULL
-author_id int FOREIGN KEY REFERENCES Author(author_id),
-genre_id int FOREIGN KEY REFERENCES Genre(genre_id)
 );
+ALTER TABLE Book_Genre
+ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book (book_id);
+ALTER TABLE Rental
+ADD CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES Genre (genre_id);
+
 
