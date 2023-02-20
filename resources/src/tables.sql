be12364345db9ec96c122a508acfbddfff7b0912
Benedict Dube <benedict.dube@vcontractor.co.za>
2023-02-20 23:28:57 +0200

    Fixed SQL errors

--- resources/src/tables.sql
+++ resources/src/tables.sql
@@ -1,3 +1,6 @@
+--CREATE DATABASE xyz;
+--USE xyz;
+
 CREATE TABLE Author(
 author_id int UNIQUE IDENTITY(1,1) NOT NULL,
 firstname varchar(50) NOT NULL,
@@ -18,8 +21,6 @@ publish_date datetime NOT NULL,
 isbn int NOT NULL,
 quantity int NOT NULL,
 );
-ALTER TABLE Book
-ADD CONSTRAINT fk_publisher_id FOREIGN KEY (publisher_id) REFERENCES Publisher (publisher_id);
 
 CREATE TABLE Member(
 member_id int UNIQUE IDENTITY(1,1) NOT NULL,
@@ -43,12 +44,6 @@ book_author_id int UNIQUE IDENTITY (1,1) NOT NULL,
 author_id int NOT NULL,
 book_id int NOT NULL,
 );
-ALTER TABLE Book_Author
-ADD CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES Author (author_id);
-
-ALTER TABLE Book_Author
-ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book (book_id);
-
 
 CREATE TABLE Rental(
 rental_id int UNIQUE IDENTITY (1,1) NOT NULL,
@@ -56,19 +51,39 @@ book_id int NOT NULL,
 member_id int NOT NULL, 
 loan_date datetime NOT NULL,
 );
-ALTER TABLE Rental
-ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book (book_id);
-ALTER TABLE Rental
-ADD CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES Member (member_id);
 
 CREATE TABLE Book_Genre(
 book_genre_id int UNIQUE IDENTITY (1,1) NOT NULL,
 genre_id int NOT NULL,
 book_id int NOT NULL
 );
+
+-- Book Genre
 ALTER TABLE Book_Genre
-ADD CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Book (book_id);
+ADD CONSTRAINT fk_book_id_genre FOREIGN KEY (book_id) REFERENCES Book (book_id);
+
+ALTER TABLE Book_Genre
+ADD CONSTRAINT fk_genre_id_genre FOREIGN KEY (genre_id) REFERENCES Genre(genre_id);
+--
+
+-- Rental --
 ALTER TABLE Rental
-ADD CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES Genre (genre_id);
+ADD CONSTRAINT fk_book_id_rental FOREIGN KEY (book_id) REFERENCES Book (book_id);
+ALTER TABLE Rental
+ADD CONSTRAINT fk_member_id_rental FOREIGN KEY (member_id) REFERENCES Member(member_id);
+--
+
+-- Book --
+ALTER TABLE Book
+ADD CONSTRAINT fk_publisher_id_book FOREIGN KEY (publisher_id) REFERENCES Publisher(publisher_id);
+--
+
+
+-- Book Author --
+ALTER TABLE Book_Author
+ADD CONSTRAINT fk_author_id_book_author FOREIGN KEY (author_id) REFERENCES Author(author_id);
+
+ALTER TABLE Book_Author
+ADD CONSTRAINT fk_book_id_book_author FOREIGN KEY (book_id) REFERENCES Book(book_id);
 
 
