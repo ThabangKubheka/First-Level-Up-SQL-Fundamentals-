1059beb873835197312b056ac3e21621b0ee1bf0
Thabang <thabangk@bbd.co.za>
2023-02-23 17:27:46 +0200

    Finally changes to the stored procedures

--- resources/src/check_out.sql
+++ resources/src/check_out.sql
@@ -7,25 +7,42 @@ BEGIN
 DECLARE 
 @available INT,
 @due_date DATE,
-@loan_date DATE 
+@loan_date DATE,
+@check INT,
+@return_date DATE
 
 
 SELECT
-    @available 
+    @return_date =return_date
+FROM
+    Rental
+WHERE
+    book_id = @book_id;
+
+SELECT
+    @available =available
 FROM
     Book
 WHERE
     book_id = @book_id;
 
-	SET 
-	@due_date = DATEADD(day, 14, @loan_date)
+	SET
+	@check =(SELECT
+    COUNT(book_id)
+FROM
+    Rental
+WHERE
+    book_id = @book_id);
 
 	SET
 	@loan_date =GETDATE()
 
-IF @available > 0 
-THEN
+	SET 
+	@due_date = DATEADD(day, 14, @loan_date)
 
+IF (@available > 0) AND (@check<=1) AND (@return_date!=Null) AND (@return_date>@due_date)
+
+BEGIN
 INSERT INTO
     Rental (book_id, member_id, loan_date, due_date)
 VALUES
@@ -33,21 +50,31 @@ VALUES
         @book_id,
         @member_id,
         @loan_date,
-        @due_date)
+        @due_date
     );
-
+	
 UPDATE
     Book
 SET
-    availble =@available  - 1
+    available = @available  - 1
 WHERE
     book_id = @book_id;
 
 PRINT
     'You checkout is successful.'
 
+END
+
 ELSE
+BEGIN
 PRINT
     'The book you are trying to checkout is not available at the moment'
+END
 
 END
+
+
+
+EXEC checkout_book 1,1
+
+select * from Rental;
